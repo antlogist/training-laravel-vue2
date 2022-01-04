e132d1b9296050aecabf69ae42f00b5ed6db2c61
antlogist <antlogist@gmail.com>
2022-01-04 22:09:11 +0300

    The second test route created

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,17 @@
-import ExampleComponent from "./components/ExampleComponent";
 import VueRouter from "vue-router";
+import ExampleComponent from "./components/ExampleComponent";
+import ExampleComponent2 from "./components/ExampleComponent2";
 
 const routes = [
   {
     path: "/",
     component: ExampleComponent,
     name: "home"
+  },
+  {
+    path: "/contact",
+    component: ExampleComponent2,
+    name: "contact"
   }
 ];
 
