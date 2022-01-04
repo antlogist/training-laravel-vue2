6b67449af2e7c0e044bb61d179f06c81e7912c7b
antlogist <antlogist@gmail.com>
2022-01-04 22:08:27 +0300

    Index component imported

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,26 @@ require('./bootstrap');
 
 import router from "./routes";
 import VueRouter from "vue-router";
+import Index from "./Index";
 
 window.Vue = require('vue').default;
 
-Vue.component(
-    'example-component',
-    require('./components/ExampleComponent.vue').default
-);
+// Vue.component(
+//     'example-component',
+//     require('./components/ExampleComponent.vue').default
+// );
+
+// Vue.component(
+//     'example-component-2',
+//     require('./components/ExampleComponent2.vue').default
+// );
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: '#app',
-    router
+    router,
+    components: {
+        "index": Index
+    }
 });
