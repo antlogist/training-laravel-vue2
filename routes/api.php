ba7f8e7f65b1c78b61d91c9df0278eb8ec93601e
antlogist <antlogist@gmail.com>
2022-01-17 20:49:33 +0300

    Bookable id route added

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->get('/user',
+    function (Request $request) {
+        return $request->user();
+    }
+);
 
 Route::get('bookables', function(Request $request) {
     return Bookable::all();
+});
+
+Route::get('bookables/{id}', function(Request $request, $id) {
+    return Bookable::findOrFail($id);
 });
