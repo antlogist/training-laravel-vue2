cb1b67fa904caa075c55e0dd3a7b1e49cac60976
antlogist <antlogist@gmail.com>
2022-01-19 22:41:23 +0300

    Form markup created

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h6 class="text-uppercase text-secondary font-weight-bolder">
+      Check availability
+    </h6>
+    <div class="row">
+
+      <div class="form-group col-6 pb-4">
+        <label for="from">From</label>
+        <input type="text" name="from" class="form-control form-control-sm" placeholder="Start date">
+      </div>
+
+      <div class="form-group col-6 pb-4">
+        <label for="to">To</label>
+        <input type="text" name="to" class="form-control form-control-sm" placeholder="End date">
+      </div>
+
+    </div>
+    <div class="d-grid">
+      <button class="btn btn-secondary">Check</button>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+label {
+  font-size: 0.7rem;
+  text-transform: uppercase;
+  font-weight: bolder;
+}
+</style>
