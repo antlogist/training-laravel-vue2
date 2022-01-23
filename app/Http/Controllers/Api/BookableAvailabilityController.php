97695396c8c0aa7fb3597723bf644805b2443377
antlogist <antlogist@gmail.com>
2022-01-23 14:37:20 +0300

    To and from validation added

--- app/Http/Controllers/Api/BookableAvailabilityController.php
+++ app/Http/Controllers/Api/BookableAvailabilityController.php
@@ -16,6 +16,12 @@ class BookableAvailabilityController extends Controller
     public function __invoke(Request $request)
     {
 
-        var_dump('Test');
+        $data = $request->validate([
+            'from' => 'required|date_format:Y-m-d|after_or_equal:now',
+            'to'   => 'required|date_format:Y-m-d|after_or_equal:from'
+        ]);
+
+        dd($data);
+
     }
 }
