7c8f5a8f44790fa716f99b6538a1b871ce2885ed
antlogist <antlogist@gmail.com>
2022-01-30 20:41:37 +0300

    fromNow filter usin moment lib added

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,20 +3,13 @@ require('./bootstrap');
 import router from "./routes";
 import VueRouter from "vue-router";
 import Index from "./Index";
+import moment from 'moment';
 
 window.Vue = require('vue').default;
 
-// Vue.component(
-//     'example-component',
-//     require('./components/ExampleComponent.vue').default
-// );
-
-// Vue.component(
-//     'example-component-2',
-//     require('./components/ExampleComponent2.vue').default
-// );
-
 Vue.use(VueRouter);
+
+Vue.filter("fromNow", value => moment(value).fromNow());
 
 const app = new Vue({
     el: '#app',
--- resources/js/bookable/ReviewList.vue
+++ resources/js/bookable/ReviewList.vue
@@ -10,7 +10,7 @@
           <div class="col-md-6 d-flex justify-content-end">{{ review.rating }}</div>
         </div>
         <div class="row">
-          <div class="col-md-12">{{ review.created_at }}</div>
+          <div class="col-md-12">{{ review.created_at | fromNow }}</div>
         </div>
         <div class="row pt-4 pb-4">
           <div class="col-md-12">
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+// import moment from 'moment';
 export default {
   props: {
     bookableId: String
@@ -39,6 +40,12 @@ export default {
     axios.get(`/api/bookables/${this.bookableId}/reviews`)
          .then(response => this.reviews = response.data.data)
          .then(()=>this.loading = false);
-  }
+  },
+  // this filter has added globally
+  // filters: {
+  //   fromNow(value) {
+  //     return  moment(value).fromNow();
+  //   }
+  // }
 }
 </script>
