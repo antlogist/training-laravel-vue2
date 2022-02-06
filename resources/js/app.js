0ab016f9013c56d9c1a73497e2640c4898ce132b
antlogist <antlogist@gmail.com>
2022-02-06 20:07:35 +0300

    Vuex store installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 import router from './routes';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import Index from './Index';
 import moment from 'moment';
 
@@ -9,10 +10,12 @@ import StarRating from './shared/components/StarRating';
 import FatalError from './shared/components/FatalError';
 import Success from './shared/components/Success';
 import ValidationErrors from './shared/components/ValidationErrors';
+import storeDefinition from './store';
 
 window.Vue = require('vue').default;
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
 
 Vue.filter('fromNow', value => moment(value).fromNow());
 
@@ -21,9 +24,12 @@ Vue.component('FatalError', FatalError);
 Vue.component('Success', Success);
 Vue.component('ValidationErrors', ValidationErrors);
 
+const store = new Vuex.Store(storeDefinition);
+
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
         'index': Index
     }
