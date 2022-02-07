b5d06358329d872c19350b3f2b43f6edecb987ff
antlogist <antlogist@gmail.com>
2022-02-07 13:25:39 +0300

    set last search dates using local storage

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -54,8 +54,8 @@ export default {
   },
   data() {
     return {
-      from: null,
-      to: null,
+      from: this.$store.state.lastSearch.from,
+      to: this.$store.state.lastSearch.to,
       loading: false,
       status: null,
     }
@@ -64,6 +64,11 @@ export default {
     check() {
       this.loading = true;
       this.errors = null;
+
+      this.$store.dispatch('setLastSearch', {
+        from: this.from,
+        to: this.to
+      });
 
       axios.get(`/api/bookables/${this.bookableId}/availability?from=${this.from}&to=${this.to}`)
       .then(response => {
