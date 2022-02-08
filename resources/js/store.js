e6a011282b43322af12a916000c3a3eba87febca
antlogist <antlogist@gmail.com>
2022-02-08 15:02:18 +0300

    Saving basket in local storage

--- resources/js/store.js
+++ resources/js/store.js
@@ -30,8 +30,11 @@ export default {
     },
     removeFromBasket(state, payload) {
       state.basket.items = state.basket.items.filter((item) => {
-        item.bookable.id !== payload;
+        return item.bookable.id !== payload;
       });
+    },
+    setBasket(state, payload) {
+      state.basket = payload;
     }
   },
   actions: {
@@ -45,6 +48,22 @@ export default {
       if(lastSearch) {
         context.commit('setLastSearch', JSON.parse(lastSearch));
       }
+
+      const basket = localStorage.getItem('basket');
+
+      if(basket) {
+        context.commit('setBasket', JSON.parse(basket));
+      }
+
+
+    },
+    addToBasket({ commit, state }, payload) {
+      commit('addToBasket', payload);
+      localStorage.setItem('basket', JSON.stringify(state.basket));
+    },
+    removeFromBasket({ commit, state }, payload) {
+      commit('removeFromBasket', payload);
+      localStorage.setItem('basket', JSON.stringify(state.basket));
     }
   }
 }
