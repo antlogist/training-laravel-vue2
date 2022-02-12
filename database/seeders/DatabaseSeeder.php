21fa5f2a8466424ea3722b742e52e3f2a26817a3
antlogist <antlogist@gmail.com>
2022-02-12 14:55:49 +0300

    Fake users added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        $this->call(UsersTableSeeder::class);
         \App\Models\Bookable::factory(10)->create();
         $this->call(BookingsTableSeeder::class);
         $this->call(ReviewsTableSeeder::class);
