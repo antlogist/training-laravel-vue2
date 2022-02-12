ea32de75e4442d39c964596da5749b1b524bb53c
antlogist <antlogist@gmail.com>
2022-02-12 22:01:12 +0300

    Login component created

---
+++ resources/js/auth/Login.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="w-50 m-auto">
+      <div class="card card-body">
+        <form>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              class="form-control"
+              v-model="email"
+              :class="[{'is-invalid': errorFor('email')}]">
+              <ValidationErrors :errors="errorFor('email')"></ValidationErrors>
+          </div>
+
+          <div class="form-group mt-3">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter your password"
+              class="form-control"
+              v-model="password"
+              :class="[{'is-invalid': errorFor('password')}]">
+              <ValidationErrors :errors="errorFor('password')"></ValidationErrors>
+          </div>
+
+          <button @click.prevent="login()" :disabled="loading" class="btn btn-primary w-100 mt-3" type="submit">Log-in</button>
+
+          <hr>
+
+          <div>
+            No account yet?
+            <router-link :to="{name: 'home'}">Register</router-link>
+          </div>
+          <div>
+            Forgotten password?
+            <router-link :to="{name: 'home'}">Register</router-link>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import validationErrors from '../shared/mixins/validationErrors'
+export default {
+  mixins: [validationErrors],
+  data() {
+    return {
+      email: null,
+      password: null,
+      loading: false
+    }
+  },
+  methods: {
+    async login() {
+      this.loading = true;
+      this.errors = null;
+
+      try {
+        await axios.get('/sanctum/csrf-cookie');
+        await axios.post('/login', {
+            email: this.email,
+            password: this.password
+        });
+        await axios.get('/user');
+      } catch(error) {
+        console.log(error);
+        this.errors = error.response &&
+        error.response.data.errors;
+      }
+
+
+      this.loading = false;
+    }
+  }
+}
+</script>
